2e9c0c335804cd77c4c85b529acdfa6007f5d775
Georgios Anastasiou <gsiou@gunet.gr>
2020-02-24 12:28:59 +0200

    authorization page with oauth metadata displayed

--- app.js
+++ app.js
@@ -21,6 +21,10 @@ app.set("view engine", "pug");
 
 app.use("/public", express.static(__dirname + "/public"));
 
+app.post('/code', (req, res) => {
+  return res.sendStatus(200);
+})
+
 app.get('/oauth', (req, res) => {
   const clientID = req.query.client_id;
   const redirectURI = req.query.redirect_uri;
@@ -28,8 +32,16 @@ app.get('/oauth', (req, res) => {
   const scope = req.query.scope;
   const response_type = req.query.response_type;
 
+  req.session.oauthData = {
+    clientID, redirectURI, state, scope, response_type
+  };
+
   console.log(clientID, redirectURI, state, scope, response_type);
   return res.render('login');
+});
+
+app.post('/login', (req, res) => {
+  return res.render('auth', { oauthData: req.session.oauthData });
 });
 
 const server = app.listen(config.port, config.address, () => {
